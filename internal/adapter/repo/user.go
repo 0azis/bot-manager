e28db9acf91f7000f072b6e43c47bd342de98399
0azis <sampledot91@gmail.com>
2024-04-26 15:09:41 +0300

    fix: delete column

--- internal/adapter/repo/user.go
+++ internal/adapter/repo/user.go
@@ -10,6 +10,7 @@ type user struct {
 	db *sqlx.DB
 }
 
+// Add user after registration
 func (u user) Insert(user domain.User) error {
 	_, err := u.db.Query(`insert into "user" (telegram_id, first_name, last_name, user_name, is_premium, language_code, avatar_url) values ($1, $2, $3, $4, $5, $6, $7) on conflict (telegram_id) do update set first_name=$2, last_name=$3, user_name=$4, is_premium=$5, language_code=$6, avatar_url=$7;`, user.TelegramID, user.FirstName, user.LastName, user.UserName, user.IsPremium, user.LanguageCode, user.AvatarUrl)
 	return err
